9b4a16c3c70db0a234147a3d41c5c6bdec914fcc
yong liang <yongliang24@gmail.com>
2018-11-04 17:48:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var temp=[];
 function takeANumber(num, name){
 
   temp = num.push(name);
-  var position[]=temp.length+1;
   var string = `Welcome, ${temp[]}. You are number 1 in line.`;
   return string;
 }
