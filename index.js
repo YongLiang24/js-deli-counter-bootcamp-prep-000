874c706d4dfce27c07490653f4f3a50329e7842d
yong liang <yongliang24@gmail.com>
2018-11-04 18:15:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-var temp=[];
 function takeANumber(num, name){
-
-  num.push(name);
+  num.push(name);//push the names to the array num
+  //use variables to dynamically output names and positions
   var string = `Welcome, ${name}. You are number ${num.length} in line.`;
   return string;
 }
