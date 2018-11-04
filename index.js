651f19a1c5d86ec179c986842bbdea0f6f3352ad
yong liang <yongliang24@gmail.com>
2018-11-04 18:28:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ num.push(name);//push the names to the array num
 //for loop to iterate the array num
 for(var i=0; i<num.length;i++)
 {
-  if(num[i]===name)//Detect the name to match the current array so we know the current position
+  if(num[i]===name)//Detect the name in the current array to trace the current position
   {
     //use variables to dynamically output names and positions
     var string = `Welcome, ${name}. You are number ${i+1} in line.`;
