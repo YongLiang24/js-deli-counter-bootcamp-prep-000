cea5024653c4f0d2115b6d25e4be5efe5b2b7767
yong liang <yongliang24@gmail.com>
2018-11-04 19:57:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,29 +24,20 @@ function nowServing(serving){
 }
 
 //Function 3 start
-/*describe('currentLine(line)', () => {
-  it('returns "The line is currently empty." if no one is in line', () => {
-    expect(currentLine([])).toEqual("The line is currently empty.");
-  });
-
-  it('says who is in line when there are people waiting', () => {
-    expect(currentLine(["Bill", "Jane", "Ann"])).toEqual("The line is currently: 1. Bill, 2. Jane, 3. Ann");*/
 function currentLine(line){
   if(line.length<1){
+    //return this when array is empty
     return "The line is currently empty."
   }
   else{
     var upArray = []; //empty array to hold the new elements
     for(var i=0; i<line.length; i++)
     {
-
+      //use for loop to push numbers and names to the empty upArray
         upArray.push(`${i+1}. ${line[i]}`);
-
-
     }
-
+    //use the join() to add a comma and space between each elements
     return `The line is currently: ${upArray.join(', ')}`;
   }
-
 
 }
