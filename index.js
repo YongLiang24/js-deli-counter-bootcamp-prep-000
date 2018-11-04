71568b22a6b8306617648c6a46fe34dd9f4249ff
yong liang <yongliang24@gmail.com>
2018-11-04 18:59:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Function 1 start
 function takeANumber(num, name){
 num.push(name);//push the names to the array num
 //for loop to iterate the array num
@@ -8,13 +9,9 @@ for(var i=0; i<num.length;i++)
     //use variables to dynamically output names and positions
     var string = `Welcome, ${name}. You are number ${i+1} in line.`;
     return string;
-  }
-}
-}
-/*t('returns an announcement about the person it is serving, and shifts the line', () => {
-  const deliLine = ["Steven", "Blake", "Avi"]
-  expect(nowServing(deliLine)).toEqual("Currently serving Steven.");
-  expect(deliLine).toEqual(["Blake", "Avi"]);*/
+  }}}
+
+//Function 2 start
 function nowServing(serving){
   if(serving.length<1){
     return "There is nobody waiting to be served!"
